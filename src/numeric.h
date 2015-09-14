a93890cada18aa7bd0ecf520d49c65ae3247e9a2
Weidong He <946516009@qq.com>
2015-09-14 20:35:39 +0800

    add algorithm in numeri.h

--- src/numeric.h
+++ src/numeric.h
@@ -40,9 +40,10 @@ OutputIterator adjacent_difference(InputIterator first, InputIterator last, Outp
 }
 
 template <typename InputIterator, typename OutputIterator, typename BinaryOperation>
-OutputIterator adjacent_difference(InputIterator first, InputIterator last, OutputIterator result, BinaryOperation binary_op)
+OutputIterator adjacent_difference(InputIterator first, InputIterator last, 
+                                   OutputIterator result, BinaryOperation binary_op)
 {
-    typedef iterator_traits<InputIterator>::value_type T;
+    typedef typename iterator_traits<InputIterator>::value_type T;
 
     if (first == last)
         return result;
@@ -55,6 +56,64 @@ OutputIterator adjacent_difference(InputIterator first, InputIterator last, Outp
         value = tmp;
     }
     return ++result;
+}
+
+
+template <typename InputIterator1, typename InputIterator2, typename T>
+T inner_product(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, T init)
+{
+    for (; first1 != last1; ++first1, ++first2)
+        init = init + (*first1 * *first2);
+    return init;
+}
+
+template <typename InputIterator1, typename InputIterator2, typename T,
+          typename BinaryOperation1, typename BinaryOperation2>
+T inner_product(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, T init,
+                BinaryOperation1 binary_op1, BinaryOperation2 binary_op2)
+{
+        for (; first1 != last1; ++first1, ++first2)
+            init = binary_op1(init, binary_op2(*first1, *first2));
+        return init;
+}
+
+template <typename InputIterator, typename OutputIterator>
+OutputIterator partial_sum(InputIterator first, InputIterator last, OutputIterator result)
+{
+    typedef typename iterator_traits<OutputIterator>::value_type T;
+
+    if (first == last)  return result;
+    *result = *first;
+
+    T value = *first;
+    while (++first != end) {
+        value = value + *first;
+        *++result = value;
+    }
+    return ++result;
+}
+
+
+template <typename InputIterator, typename OutputIterator, typename BinaryOperation>
+OutputIterator partial_sum(InputIterator first, InputIterator last, OutputIterator result, BinaryOperation binary_op)
+{
+    typedef typename iterator_traits<OutputIterator>::value_type T;
+
+    if (first == last)  return result;
+    *result = *first;
+    T value = *first;
+    while (++first != last) {
+        value = binary_op(value, *first);
+        *++result = value;
+    }
+    return ++result;
+}
+
+template <typename ForwardIterator, typename T>
+void iota(ForwardIterator first, ForwardIterator last, T value)
+{
+    while (first != last)
+        *first++ = value++;
 }
 
 } // namespace tinyc
