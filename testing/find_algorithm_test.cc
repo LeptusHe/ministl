79433164e07f7c0357d62ea2639ef68f0d44f551
heweidong <heweidong526@gmail.com>
2015-12-24 12:05:03 +0800

    add test for find algorithms

---
+++ testing/find_algorithm_test.cc
@@ -0,0 +1,33 @@
+#include <vector>
+#include <random>
+#include <algorithm>
+
+#include "gtest\gtest.h"
+#include "algorithm.h"
+
+namespace ministl{
+
+class FindAlgorithmTest : public testing::Test {
+public:
+  using RandomEngine = std::default_random_engine;
+public:
+  static RandomEngine random_engine;
+};
+
+FindAlgorithmTest::RandomEngine FindAlgorithmTest::random_engine;
+
+TEST_F(FindAlgorithmTest, findTest)
+{
+  const int TEST_TIMES = 1000;
+  const int VECTOR_NUMS = 1000;
+  RandomEngine num_productor;
+  int need_find = num_productor();
+  std::vector<int> seq(VECTOR_NUMS);
+  
+  for (int i = 0; i < TEST_TIMES; ++i) {
+    std::generate(seq.begin( ), seq.end( ), random_engine);
+    EXPECT_EQ(ministl::find(seq.begin( ), seq.end( ), need_find), std::find(seq.begin( ), seq.end( ), need_find));
+  }
+}
+
+} // namespace ministl
