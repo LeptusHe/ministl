f5869c1b9e405624c51d05e04b0f275ae4ff2fb8
heweidong <heweidong526@gmail.com>
2015-12-24 23:33:41 +0800

    add algorithms for set

--- src/functools.h
+++ src/functools.h
@@ -1,8 +1,50 @@
 #ifndef MINISTL_FUNCTOOLS_H
 #define MINISTL_FUNCTOOLS_H
 
+#include <algorithm>
+
 namespace ministl {
 
+// algorithms for set
+// names of algorithm: all_of, any_of, none_of
+
+template <typename InputIterator, typename Predicate>
+bool all_of(InputIterator first, InputIterator last, Predicate pred)
+{
+  bool flag = true;
+
+  for (; first != last && flag; ++first) {
+    if (!pred(*first))
+      flag = false;
+  }
+  return flag;
+}
+
+template <typename InputIterator, typename Predicate>
+bool any_of(InputIterator first, InputIterator last, Predicate pred)
+{
+  bool flag = false;
+
+  for (; first != last && !flag; ++first) {
+    if (pred(*first))
+      flag = true;
+  }
+  return flag;
+}
+
+template <typename InputIterator, typename Predicate>
+bool none_of(InputIterator first, InputIterator last, Predicate pred)
+{
+  bool flag = true;
+
+  for (; first != last && flag; ++first) {
+    if (pred(*first))
+      flag = false;
+  }
+  return flag;
+}
+
+// names of algorithms: map
 template <typename ForwardIterator, typename Operation>
 inline void map(ForwardIterator first, ForwardIterator last, Operation op)
 {
