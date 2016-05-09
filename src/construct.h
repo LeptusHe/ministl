7e0457142e33af6ec3a2c736cefc61397f127390
TailsKuka <heweidong526@gmail.com>
2016-05-09 23:41:41 +0800

    add destory function for primary type

--- src/construct.h
+++ src/construct.h
@@ -8,32 +8,41 @@
 namespace ministl {
 
 template <class T1, class T2>
-inline void construct(T1 *p, const T2 &value) { new(p)T1(value); }
+inline void construct(T1 *p, const T2 &value) { new ((void*)p) T1(value); }
 
 template <typename T>
 inline void destory(T* pointer) { pointer->~T( ); }
 
 template <typename ForwardIterator>
-inline void destory(ForwardIterator first, ForwardIterator last) { __destory(first, last, value_type(first)); }
+inline void destory(ForwardIterator first, ForwardIterator last)
+{
+  do_destory(first, last, value_type(first));
+}
 
 template <class ForwardIterator, class T>
-inline void __destory(ForwardIterator first, ForwardIterator last, T*)
+inline void do_destory(ForwardIterator first, ForwardIterator last, T*)
 {
-    typedef typename __type_traits<T>::has_trivial_destructor trivial_destructor;
-    __destory_aux(first, last, trivial_destructor( ));
+    typedef typename type_traits<T>::has_trivial_destructor trivial_destructor;
+    destory_aux(first, last, trivial_destructor( ));
 }
 
 template <typename ForwardIterator>
-inline void __destory_aux(ForwardIterator first, ForwardIterator last, __false_type)
+inline void destory_aux(ForwardIterator first, ForwardIterator last, false_type)
 {
     for (; first < last; ++first)
         destory(&*first);
 }
 
 template <typename ForwardIterator>
-inline void __destory_aux(ForwardIterator first, ForwardIterator last, __true_type) { }
-inline void destory(char *, char *) { }
-inline void destory(wchar_t *, wchar_t *) { }
+inline void destory_aux(ForwardIterator first, ForwardIterator last, true_type)
+{ }
+
+inline void destory(char*, char*) { }
+inline void destory(int*, int*) { }
+inline void destory(long*, long*) { }
+inline void destory(float*, float*) { }
+inline void destory(double*, double*) { }
+inline void destory(wchar_t*, wchar_t*) { }
 
 } // namespace ministl
 
