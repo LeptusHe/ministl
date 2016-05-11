b970da1f86aaaef213199fa4213bd6bf5411c831
TailsKuka <heweidong526@gmail.com>
2016-05-11 23:34:03 +0800

    add test for for_each algorithm

--- testing/functools_test.cc
+++ testing/functools_test.cc
@@ -1,13 +1,15 @@
-#include <vector>
 #include <algorithm>
 #include <functional>
+#include <string>
+
+#include "vector.h"
+#include "functools.h"
 
 #include "gtest/gtest.h"
-#include "functools.h"
 
 namespace ministl {
 
-TEST(FunctoolsTest, MapTest)
+TEST(FunctoolsTest, map_test)
 {
   int data = 0;
   std::function<int(int)> square = [] (int n)->int { return n * n; };
@@ -21,6 +23,18 @@ TEST(FunctoolsTest, MapTest)
   auto mismatch_point = std::mismatch(test_data.begin( ), test_data.end( ), excepted_data.begin( ), pred);
   auto excepted_result = std::make_pair<decltype(test_data.end( )), decltype(excepted_data.end( ))>(test_data.end( ), excepted_data.end( ));
   EXPECT_EQ(excepted_result, mismatch_point) << *mismatch_point.first << " " << *mismatch_point.second << std::endl;
+}
+
+TEST(FunctoolsTest, for_each_test)
+{
+  const int vec_size = 10;
+  ministl::vector<int> int_vec(vec_size);
+  int start_value = 0, ans = 0;
+  ministl::generate(int_vec.begin( ), int_vec.end( ),
+                    [&start_value, &ans]( ) { ans += start_value; return start_value++; });
+  int test_ans = 0;
+  ministl::for_each(int_vec.begin( ), int_vec.end( ), [&test_ans](int x) { test_ans += x; });
+  EXPECT_EQ(ans, test_ans);
 }
 
 } // namespace ministl
