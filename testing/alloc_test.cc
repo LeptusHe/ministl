016ee115c134bedaaae7751b69c77c3819936a78
TailsKuka <heweidong526@gmail.com>
2016-05-13 20:12:32 +0800

    add test for alloc

---
+++ testing/alloc_test.cc
@@ -0,0 +1,44 @@
+#include "alloc.h"
+#include "gtest/gtest.h"
+
+namespace ministl {
+
+// test cases for template malloc_alloc_template
+TEST(MallocAllocTemplateTEST, allocate_deallocate_reallocate_test)
+{
+  malloc_alloc allocator;
+  const int constant_value = 10;
+  int* int_value = (int*)allocator.allocate(sizeof(*int_value));
+  *int_value = constant_value;
+  EXPECT_EQ(constant_value, *int_value); // test for allocate
+  
+  // test for reallocate
+  const double constant_double = 3.141592654;
+  double* double_value = (double*)allocator.reallocate(int_value, sizeof(*int_value), sizeof(double));
+  *double_value = constant_double;
+  EXPECT_EQ(constant_double, *double_value);
+
+  // test for deallocate
+  allocator.deallocate(double_value, sizeof(double));
+}
+
+// test cases for default_alloc_template
+TEST(DefaultAllocTemplateTEST, allocate_deallocate_reallocate_test)
+{
+  alloc allocator;
+  const int constant_value = 10;
+  int* int_value = (int*)allocator.allocate(sizeof(*int_value));
+  *int_value = constant_value;
+  EXPECT_EQ(constant_value, *int_value); // test for allocate
+
+                                         // test for reallocate
+  const double constant_double = 3.141592654;
+  double* double_value = (double*)allocator.reallocate(int_value, sizeof(*int_value), sizeof(double));
+  *double_value = constant_double;
+  EXPECT_EQ(constant_double, *double_value);
+
+  // test for deallocate
+  allocator.deallocate(double_value, sizeof(double));
+}
+
+} // namespace ministl
