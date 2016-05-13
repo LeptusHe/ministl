a33b3f3143fe8175b7df9e13c7f4ddd53ec57f9f
TailsKuka <heweidong526@gmail.com>
2016-05-13 20:06:24 +0800

    add test for stack

---
+++ testing/stack_test.cc
@@ -0,0 +1,37 @@
+#include "stack.h"
+#include "gtest/gtest.h"
+#include <stack>
+
+namespace ministl {
+
+TEST(StackTest, construct_destroy_size_capacity_capacity_test)
+{
+  const int size = 100;
+  ministl::stack<int> int_stack;
+  ministl::stack<double> double_stack(size);
+  ministl::stack<float> float_stack(size, 2.0);
+
+  EXPECT_EQ(0, int_stack.size());
+  EXPECT_EQ(size, double_stack.size());
+  EXPECT_EQ(size, float_stack.size( ));
+  EXPECT_TRUE(int_stack.empty( ));
+  EXPECT_FALSE(double_stack.empty( ));
+  EXPECT_FALSE(float_stack.empty( ));
+}
+
+TEST(StackTest, push_pop_top_test)
+{
+  const int size = 10;
+  ministl::stack<int> int_stack;
+  for (int i = 0; i < size; ++i)
+    int_stack.push(i * i);
+  EXPECT_EQ(size, int_stack.size( ));
+  EXPECT_FALSE(int_stack.empty( ));
+  EXPECT_EQ((size - 1) * (size - 1), int_stack.top( ));
+  while (!int_stack.empty( ))
+    int_stack.pop( );
+  EXPECT_EQ(0, int_stack.size( ));
+  EXPECT_TRUE(int_stack.empty( ));
+}
+
+} // namespace ministl
