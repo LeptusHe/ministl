da02562c5cf72a555539581b93431294fd016148
KukaTails <heweidong526@gmail.com>
2016-06-01 21:55:25 +0800

    add algorithm with predicate

--- src/heap.h
+++ src/heap.h
@@ -29,6 +29,33 @@ void do_push_heap(RandomAccessIterator first, Distance hole_index, Distance top_
 }
 
 
+
+template <typename RandomAccessIterator, typename Compare>
+inline void push_heap(RandomAccessIterator first, RandomAccessIterator last, Compare comp)
+{
+  push_heap_aux(first, last, comp, distance_type(first), value_type(first));
+}
+
+template <typename RandomAccessIterator, typename Compare, typename Distance, typename T>
+inline void push_heap_aux(RandomAccessIterator first, RandomAccessIterator last, Compare comp, Distance*, T*)
+{
+  do_push_heap(first, Distance((last - first) - 1), Distance(0), T(*(last - 1)), comp);
+}
+
+template <typename RandomAccessIterator, typename Distance, typename T, typename Compare>
+void do_push_heap(RandomAccessIterator first, Distance hole_index, Distance top_index, T value, Compare comp)
+{
+  Distance parent = (hole_index - 1) / 2;
+  while (hole_index > top_index && comp(*(first + parent), value)) {
+    *(first + hole_index) = *(first + parent);
+    hole_index = parent;
+    parent = (hole_index - 1) / 2;
+  }
+  *(first + hole_index) = value;
+}
+
+
+
 template <typename RandomAccessIterator>
 inline void pop_heap(RandomAccessIterator first, RandomAccessIterator last)
 {
@@ -70,13 +97,47 @@ void adjust_heap(RandomAccessIterator first, Distance hole_index, Distance len, 
 }
 
 
-template <typename RandomAccessIterator>
-void sort_heap(RandomAccessIterator first, RandomAccessIterator last)
+
+template <typename RandomAccessIterator, typename Compare>
+inline void pop_heap(RandomAccessIterator first, RandomAccessIterator last, Compare compare)
 {
-  while (last - first > 1) {
-    ministl::pop_heap(first, last--);
-  }
+  pop_heap_aux(first, last, value_type(first), comp);
 }
+
+template <typename RandomAccessIterator, typename T, typename Compare>
+inline void pop_heap_aux(RandomAccessIterator first, RandomAccessIterator last, T*, Compare comp)
+{
+  do_pop_heap(first, last - 1, last - 1, T(*(last - 1)), comp, distance_type(first));
+}
+
+template <typename RandomAccessIterator, typename T, typename Compare, typename Distance>
+inline void do_pop_heap(RandomAccessIterator first, RandomAccessIterator last, RandomAccessIterator result,
+                        T value, Compare comp, Distance*)
+{
+  *result = *first;
+  adjust_heap(first, Distance(0), Distance(last - first), value, comp);
+}
+
+template <typename RandomAccessIterator, typename Distance, typename T, typename Compare>
+void adjust_heap(RandomAccessIterator first, Distance hole_index, Distance len, T value, Compare comp)
+{
+  Distance top_index = hole_index;
+  Distance second_child = 2 * hole_index + 2;
+
+  while (second_child < len) {
+    if (comp(*(first + second_child), *(first + (second_child - 1))))
+      second_child--;
+    *(first + hole_index) = *(first + second_child);
+    hole_index = second_child;
+    second_child = 2 * (second_child + 1);
+  }
+  if (second_child == len) {
+    *(first + hole_index) = *(first + (second_child - 1));
+    hole_index = second_child - 1;
+  }
+  do_push_heap(first, hole_index, top_index, value);
+}
+
 
 
 template <typename RandomAccessIterator>
@@ -95,6 +156,48 @@ void do_make_heap(RandomAccessIterator first, RandomAccessIterator last, T*, Dis
     adjust_heap(first, parent, len, T(*(first + parent)));
     if (parent == 0) return;
     parent--;
+  }
+}
+
+
+
+template <typename RandomAccessIterator, typename Compare>
+inline void make_heap(RandomAccessIterator first, RandomAccessIterator last, Compare comp)
+{
+  do_make_heap(first, last, comp, value_type(first), distance_type(first));
+}
+
+template <typename RandomAccessIterator, typename Compare, typename Distance, typename T>
+void do_make_heap(RandomAccessIterator first, RandomAccessIterator last, Compare comp, T*, Distance*)
+{
+  if (last - first < 2) return;
+  Distance len = last - first;
+  Distance parent = (len - 2) / 2;
+  while (true) {
+    adjust_heap(first, parent, len, T(*(first + parent)), comp);
+    if (parent == 0) return;
+    parent--;
+  }
+}
+
+
+
+
+template <typename RandomAccessIterator>
+void sort_heap(RandomAccessIterator first, RandomAccessIterator last)
+{
+  while (last - first > 1) {
+    ministl::pop_heap(first, last--);
+  }
+}
+
+
+
+template <typename RandomAccessIterator, typename Compare>
+void sort_heap(RandomAccessIterator first, RandomAccessIterator last, Compare comp)
+{
+  while (last - first > 1) {
+    ministl::pop_heap(first, last--, comp);
   }
 }
 
