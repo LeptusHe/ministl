eb7be9bd962348f96a45551f7b72286f2870a0c1
KukaTails <heweidong526@gmail.com>
2016-07-24 23:45:47 +0800

    add hashtable iterator

--- src/hashtable.h
+++ src/hashtable.h
@@ -1,16 +1,27 @@
 #ifndef HASHTABLE_H
 #define HASHTABLE_H
 
+namespace ministl {
+
 template <typename Value>
-struct __hashtable_node {
-    __hashtable_node *node;
-    Value val;
+struct hashtable_node {
+  HashtableNode* next;
+  Value val;
+};
+
+template <typename Value, typename Key, typename HashFunc,
+          typename ExtractKey, typename EqualKey, typename Alloc>
+struct hashtable_iterator {
+  using hashtable = hashtable<Value, Key, HashFunc, ExtractKey, EqualKey, Alloc>;
+  using iterator = hashtable_iterator<Value, Key, HashFunc, ExtractKey, EqualKey, Alloc>;
+  using const_iterator = hashtable_const_iterator<Value, Key, HashFunc, ExtractKey, EqualKey, Alloc>;
+  using node = hashtable_node<Value>;
 };
 
 
 
 
 
-
+} // namespace ministl
 
 #endif
